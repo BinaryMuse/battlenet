8974bd40bf129638138c5de5fb492519a626b9eb
Espen A. Nilsen <post@espennilsen.net>
2011-08-04 10:14:40 +0200

    Added missing end

--- lib/battlenet/api/auction.rb
+++ lib/battlenet/api/auction.rb
@@ -9,6 +9,7 @@ module Battlenet
         data = @api.make_api_call 'auction/data/' +slug
         if data["files"]["lastModified"] > lastModified
           data = @api.make_api_call data["files"]["url"]
+        end
       end
     end
   end
