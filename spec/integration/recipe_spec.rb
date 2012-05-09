2cbc1dbe82ea831cb128fd3abb668a616242af3a
Brandon Tilley <brandon@brandontilley.com>
2012-05-08 21:57:59 -0700

    Add specs for recipe resource

--- spec/integration/recipe_spec.rb
+++ spec/integration/recipe_spec.rb
@@ -4,10 +4,9 @@ describe Battlenet::Modules::Recipe do
   let(:api) { Battlenet.new }
 
   it "fetches recipe data" do
-    VCR.use_cassette('recipe') do
-      recipe = api.recipe '33994'
-      recipe['name'].should == "Enchant Gloves - Precise Strikes"
-      recipe['profession'].should == "Enchanting"
+    VCR.use_cassette('recipe_27984') do
+      recipe = api.recipe '27984'
+      recipe['name'].should == "Enchant Weapon - Mongoose"
     end
   end
 end
